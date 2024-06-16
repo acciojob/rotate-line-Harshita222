934c9b0a816e20b266fddf4ac2bf633ea971cbe9
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-06-16 21:56:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+// const id= document.getElementById("line")
+let deg = 0;
+setInterval(function() {
+    document.getElementById('line').style.transform = "rotate(" + deg + "deg)";
+    deg += 2;
+}, 20);
